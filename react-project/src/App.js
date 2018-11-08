70eb0187d5b93c1b1719975fab399a5e85688b19
Kaloyan Tomov <kaloyan.tomov@ad.mentormate.bg>
2018-11-08 16:37:39 +0200

    Sign in page ui

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,26 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+      <form method="post">
+        <div class="box">
+          <h1>Sign in</h1>
+
+          <input type="email" name="email" placeholder="Email" class="email" />
+
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            class="email"
+          />
+
+          <a href="#">
+            <div class="btn">Sign In</div>
           </a>
-        </header>
-      </div>
+
+          <a href="#">
+            <div id="btn2">Sign Up</div>
+          </a>
+        </div>
+      </form>
     );
   }
 }
