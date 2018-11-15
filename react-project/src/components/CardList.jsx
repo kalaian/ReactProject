cc18129cad331946999fa24af6712cfa8d0a0d7f
Kaloyan Tomov <kaloyan.tomov@ad.mentormate.bg>
2018-11-15 19:40:04 +0200

    Added Item name and description

--- react-project/src/components/CardList.jsx
+++ react-project/src/components/CardList.jsx
@@ -3,26 +3,23 @@ import ItemCard from './ItemCard';
 
 export default class CardList extends Component {
 	state = {
-    // characters: [ { name: 'pesho', id: 1 }, { name: 'gosho', id: 2 }, { name: 'diamand', id: 3 } ]
-   results: []
-  };
-  componentWillMount() {
-    this.loadCharacters();
-  }
+		results: []
+	};
+	componentWillMount() {
+		this.loadCharacters();
+	}
 
-  loadCharacters= () => {
-    fetch('https://gateway.marvel.com/v1/public/characters?apikey=80a0c3d7955eb762515f55d1412ed8cb')
-      .then(response => response.json())
-      .then(json => this.setState({
-        results: json.data.results
-      }))
-  }
+	loadCharacters = () => {
+		fetch('https://gateway.marvel.com/v1/public/characters?apikey=80a0c3d7955eb762515f55d1412ed8cb')
+			.then((response) => response.json())
+			.then((json) =>
+				this.setState({
+					results: json.data.results
+				})
+			);
+	};
 
 	render() {
-		return (
-			<div>
-		<ItemCard>{this.state.results.map((result) => {})}</ItemCard>
-			</div>
-		);
+		return <div>{this.state.results.map((result) => <ItemCard {...result} />)}</div>;
 	}
 }
